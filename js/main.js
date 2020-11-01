600025dd21932d4cdcc274bef730d13fc985b3ca
CJ-Yang <cj.yang0225@gmail.com>
2020-11-01 18:32:17 +0800

    add_task-expansion 切版

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-let links = document.querySelectorAll(".nav-item-link");
+const links = document.querySelectorAll(".nav-item-link");
 
 links.forEach(link => {
   link.addEventListener("click", function() {
     links.forEach(link => link.classList.remove("active"));
     link.classList.add("active");
   });
+});
+
+const btn_more = document.querySelector(".btn_more");
+height: 0;
+const expansion = document.querySelector(".add_task-expansion");
+
+btn_more.addEventListener("click", function() {
+  console.log(this);
+  expansion.classList.toggle("is_expanded");
 });
