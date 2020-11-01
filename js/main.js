78586e26b786f71346136700d46e470e034c12e4
CJ-Yang <cj.yang0225@gmail.com>
2020-11-01 20:00:48 +0800

    add_task-expansion 樣式更新

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 const links = document.querySelectorAll(".nav-item-link");
+const btn_more = document.querySelector(".btn_more");
+const expansion = document.querySelector(".add_task-expansion");
+const items = JSON.parse(localStorage.getItem("items")) || []; // 資料獲取時為字串，需要轉換物件 (注意無資料的處理)
 
 links.forEach(link => {
   link.addEventListener("click", function() {
@@ -6,10 +9,6 @@ links.forEach(link => {
     link.classList.add("active");
   });
 });
-
-const btn_more = document.querySelector(".btn_more");
-height: 0;
-const expansion = document.querySelector(".add_task-expansion");
 
 btn_more.addEventListener("click", function() {
   console.log(this);
